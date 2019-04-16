aea8f2408f901a59cec91ba847496be2715cc5ea
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-16 10:57:15 -0600

    Add selected colors component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app"> 
+    <SelectedColors 
+      v-if="selectedColors.length > 0"
+      :selectedColors="selectedColors"
+      @fetch-art="fetchArt">
+    </SelectedColors>
     <div class="colorArea">
       <Colors 
         v-for="(color, index) in colors" 
         :key="index" 
         :color="color"
-        @add-color="addColor"></Colors>
+        @add-color="addColor">
+      </Colors>
     </div>
     <button @click="fetchColors">Fetch More Colors</button>
   </div>
@@ -13,24 +19,27 @@
 
 <script>
 import Colors from './components/Colors.vue';
+import SelectedColors from './components/SelectedColors.vue';
 import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    Colors
+    Colors,
+    SelectedColors
   },
   data() {
     return{
       colors: [],
       nextColors: '',
       selectedColors: [],
-      allColors: false
+      allColors: false,
+      art: []
     }
   },
   mounted() {
     axios
-      .get(`https://api.harvardartmuseums.org/color?apikey=${process.env.VUE_APP_API_KEY}`)
+      .get(`https://api.harvardartmuseums.org/color?apikey=${process.env.VUE_APP_API_KEY}&size=150`)
       .then(response => {
         this.colors = response.data.records
         this.nextColors = 2
@@ -53,6 +62,15 @@ export default {
       if(!this.selectedColors.includes(color)){
         this.selectedColors.push(color)
       }
+    },
+    fetchArt: function() {
+      const colorString = this.selectedColors.join('|')
+      console.log(colorString)
+      axios
+        .get(`https://api.harvardartmuseums.org/object?apikey=${process.env.VUE_APP_API_KEY}&color=${colorString}`)
+        .then(response => {
+          console.log(response)
+        })
     }
   }
 }
@@ -63,4 +81,6 @@ export default {
     display: flex;
     flex-wrap: wrap;
   }
+
+  
 </style>
---
+++ src/components/SelectedColors.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>SelectedColors</h1>
+      <div class="selectedColorArea"> 
+        <div 
+          class="selectedColor"
+          v-for="(color, i) in selectedColors"
+          :key="i"
+          :style="{backgroundColor: color}">
+        </div>
+      </div>
+      <button
+        @click="$emit('fetch-art')">Find Art</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectedColors',
+  props: {
+    selectedColors: Array
+  }
+}
+</script>
+
+<style scoped>
+  .selectedColorArea {
+    display: flex;
+  }
+
+  .selectedColor {
+    height: 50px;
+    width: 50px;
+    border: 2px solid black;
+    border-radius: 50%;
+    margin: 2px;
+  }
+</style>
