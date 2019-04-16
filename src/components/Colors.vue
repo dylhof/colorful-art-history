dada5c42064d39037e07edcfd2ee5f204c9be886
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-15 18:33:02 -0600

    Clean up the template

---
+++ src/components/Colors.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    Colors
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Colors'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
