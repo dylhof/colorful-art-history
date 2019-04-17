ce32054f01eb6dd636b936939deb6ff2b6a3d490
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-16 22:20:39 -0600

    Add divs, classes and styles to App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <header><h1>Colorful Art History</h1></header>
     <div class="selectedColors">
       <SelectedColors
         v-if="selectedColors.length > 0"
@@ -8,12 +9,17 @@
       ></SelectedColors>
     </div>
     <div v-if="view === 'colors'" class="colorArea">
-      <Colors v-for="(color, index) in colors" :key="index" :color="color" @add-color="addColor"></Colors>
+      <p class="blurb">Select as many colors as you like, then find how colorful Art History can be! </p>
+      <div class="colorArea-div">
+        <Colors v-for="(color, index) in colors" :key="index" :color="color" @add-color="addColor"></Colors>
+      </div>
     </div>
     <div v-else-if="view === 'art'" >
-      <button @click="backToColors">Back To Colors</button>
       <div class="artArea">
-        <Art v-for="(artObject, index) in art" :key="index" :artObject="artObject"></Art>
+        <button @click="backToColors">Back To Colors</button>
+        <div class="paintingArea">
+          <Art v-for="(artObject, index) in art" :key="index" :artObject="artObject"></Art>
+        </div>
       </div>
     <button @click="fetchArt">Fetch More Art</button>
     </div>
@@ -74,7 +80,8 @@ export default {
         )
         .then(response => {
           response.data.records.forEach(record => {
-            if (record.images !== undefined) {
+
+            if (record.images !== undefined && record.images.length !== 0) {
               const images = [];
               const people = [];
               record.images.forEach(image => images.push(image.baseimageurl));
@@ -100,15 +107,60 @@ export default {
 </script>
 
 <style>
-.colorArea,
-.artArea {
+@import url('https://fonts.googleapis.com/css?family=Arvo');
+
+* {
+  font-family: 'Arvo', serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  height: 100px;
+  position: fixed;
+  background-color: white;
+  width: 100%;
+}
+
+.blurb {
+  margin: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+
+.colorArea-div,
+.paintingArea {
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
+}
+
+.colorArea {
+  padding-top: 175px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.artArea {
+  padding-top: 150px;
 }
 
 .selectedColors {
   position: fixed;
   background-color: white;
   width: 100%;
+  top: 100px;
+}
+
+button {
+  height: 30px;
+
+}
+
+h1{
+  margin: 20px;
 }
 </style>
